11394d2a906265d44e073126104827d3e8fca017
chendonglin <1556964226@qq.co>
2023-03-17 22:12:53 +0800

    user center

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,7 @@ Page({
       "https://img1.baidu.com/it/u=2217702911,1536797179&fm=253&fmt=auto&app=138&f=JPEG?w=1250&h=500", "https://img2.baidu.com/it/u=3948194614,644501665&fm=253&fmt=auto&app=138&f=JPEG?w=1373&h=500"
     ],
     middleContent: [{
-      "iconPath": "/img/news.png",
+       "iconPath": "/img/news.png",
       "text": "新闻"
     }, {
       "iconPath": "/img/yule.png",
